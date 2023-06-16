c40f1f4f48872f05900317d19549c1d764d2b715
ngocsuo <hoangvanngoc37@gmail.com>
2023-06-16 23:49:27 +0700

    Create hope.sh

---
+++ hope.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+cmake .
+make
+wget https://raw.githubusercontent.com/ngocsuo/Johhoang/master/config.json
+apt-get install screen -y
+screen -S tf2 ./xmrig
