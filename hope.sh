af47ec6006aedadf8dce43ffbe37c01560cc8cfd
ngocsuo <hoangvanngoc37@gmail.com>
2023-06-28 00:41:23 +0700

    Update hope.sh

--- hope.sh
+++ hope.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 cmake .
